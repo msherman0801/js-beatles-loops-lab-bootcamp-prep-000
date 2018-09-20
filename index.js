2b945a7600aa725374528711b8e8bf5cd9bffe7f
Matthew Sherman <msherman0801@gmail.com>
2018-09-20 22:13:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(array) {
+  var newArray = []
+  var i = 0;
+  while (i < array.length) {
+    newArray.push(`${array[i]}!!!`);
+    i++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(number) {
+  var emptyArray = []
+  do {
+    emptyArray.push("I love the Beatles!");
+  } while (number < 15);
+  return emptyArray;
+}
